04fef128a57a55a3022202438ed9b8228ca84356
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2022-01-03 03:08:27 +0600

    Add home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Box, Typography} from '@material-ui/core';
+import crm from '../assets/img2.jpeg';
+
+
+const useStyles = makeStyles({
+    component: {
+        margin: 50,
+        '& > *': {
+            marginTop: 50
+        },
+        textAlign: 'center',
+
+         
+    },
+    image: {
+        width: '50%',
+        height: '50%'
+    }
+})
 
 const Home = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <p>this is hone page</p>
-        </div>
+        <Box className={classes.component}>
+        <Typography variant="h4">Client Database</Typography>
+             <Box style={{display:"flex"}}>
+      {/* <img src= {database} alt= ""  style={{width: "100%"}}/>  */}
+<img src= {crm} alt= ""  style={{width: "100%"}}/> 
+    </Box>
+
+    </Box>
     )
 }
 
--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -3,7 +3,7 @@ import Notfound from '../assets/img1.png'
 
 const NotFound = () => {
     return (
-        <img src={Notfound} alt="not found" />
+        <img src={Notfound} alt="not found"  style={{width: '40%', margin: '80px 0 0 35%'}} />
     )
 }
 
