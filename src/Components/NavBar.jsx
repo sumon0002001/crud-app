9b9f8355dddfe6695d6754785e87b2318c32a997
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2022-01-03 00:32:32 +0600

    Add navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const NavBar = () => {
+  
+    return (
+        <AppBar>
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar
